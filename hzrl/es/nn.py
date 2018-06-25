10b27da7c95e84b73de186f977add808644ccd7a
bowen <bxw229@case.edu>
2018-06-25 13:45:42 -0400

    update

--- hzrl/es/nn.py
+++ hzrl/es/nn.py
@@ -1,24 +1,68 @@
 import autograd.numpy as np 
 
+def relu(x):
+	return np.maximum(x, 0)
+
+def passthru(x):
+	return x
+
+def tanh(x):
+	return np.tanh(x)
+
 class NeuralNetwork():
 
 	def __init__(self, input_dim=1,
 				 output_dim=20,
 				 units=[20,20,20],
 				 activations=["relu", "relu", "passthru"]):
-	
+
 		self.input_dim = input_dim
 		self.output_dim = output_dim
 		self.units = units
 		self.num_layers = len(units)
+		
+		self.activations = []
+		for act in activations:
+			if act == "relu":
+				self.activations += [relu]
+			elif act == "passthru":
+				self.activations += [passthru]
+			else:
+				self.activations + [tanh]
 
+		self.shapes = []
+		unit_list = [input_dim]+self.units+[output_dim]
+		for l in range(self.num_layers):
+			self.shapes += [(unit_list[2*l], unit_list[2*l+1])]
 
-	def get_params(self):
-		pass
+		self.weight = []
+		self.bias = []
+		self.parameter_count = 0
+		idx = 0
+		for shape in self.shapes:
+			self.weight.append(np.zeros(shape=shape))	
+			self.bias.append(np.zeros(shape=shape[1]))
+			self.parameter_count += (np.product(shape) + shape[1])
+			idx += 1
 
-	def set_params(self):
-		pass
+	def set_weights(self, model_params):
+		pointer = 0
+		for i in range(len(self.shapes)):
+			w_shape = self.shapes[i]
+			b_shape = self.shapes[i][1]
+			s_w = np.product(w_shape)
+			s = s_w + b_shape
+			chunk = np.array(model_params[pointer:pointer+s])
+			self.weight[i] = chunk[:s_w].reshape(w_shape)
+			self.bias[i] = chunk[s_w:].reshape(b_shape)
+			pointer += s
 
-	def predict(self):
-		return None
-
+	def predict(self, X):
+		h = np.array([X]).flatten()
+		num_layers= len(self.weight)
+		for i in range(num_layers):
+			w = self.weight[i]
+			b = self.bias[i]
+			h = np.matmul(h, w) + b
+			h = self.activations[i](h)
+		return h
---
+++ hzrl/rabbit_train.py
@@ -0,0 +1,18 @@
+
+from es import nn, oes
+
+
+class policy():
+
+	def __init__(self, theta=None):
+		self.theta = theta
+
+	def get_action(self, state):
+		return None
+
+
+
+
+while Train:
+
+	solutions = es.ask()
