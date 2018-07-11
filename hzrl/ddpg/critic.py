e2dc46de34d2088ad2e556a4d14efddc9981b430
Bobeye <bxw229@case.edu>
2018-07-11 17:33:07 -0400

    add critic

--- hzrl/ddpg/critic.py
+++ hzrl/ddpg/critic.py
@@ -1,4 +1,5 @@
 from nn import NeuralNetwork
+import autograd.numpy as np
 
 class CriticNetwork():
 
--- hzrl/ddpg/nn.py
+++ hzrl/ddpg/nn.py
@@ -1,4 +1,4 @@
-import numpy as np
+import autograd.numpy as np
 
 def relu(x):
 	return np.maximum(x, 0)
