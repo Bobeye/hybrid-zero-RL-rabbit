827cbf439b146bb101c49fbb76b1ad1b549966c1
Bobeye <bxw229@case.edu>
2018-08-14 18:19:22 -0400

    init icra submission prep

---
+++ hzd_rl/rabbit_tools/reward.py
@@ -0,0 +1,23 @@
+import trajectory
+
+"""Customized Reward Func"""
+def get_reward(observation, desired_vel, velocity, mode="default"):
+	if mode == "default":	
+		if abs(velocity-desired_vel) <= 0.1:
+			if velocity <= desired_vel:
+				velocity_reward = (velocity / desired_vel)**2
+			else:
+				velocity_reward = (desired_vel / velocity)**2
+		else:
+			velocity_reward = 0
+
+		lf_clr = trajectory.left_foot_height(observation[0:7])
+		rf_clr = trajectory.right_foot_height(observation[0:7])
+		clearance_reward = 0.5*(lf_clr-rf_clr)**2
+		
+		reward = 0.1 * (10 * velocity_reward + clearance_reward)
+	else:
+		raise NotImplementedError
+
+	return reward
+
