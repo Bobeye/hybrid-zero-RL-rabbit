e5796020988bf5364874cf3a58c570ea353a7c7b
gcastillom90 <g.castillom90@gmail.com>
2018-08-28 12:11:11 -0400

    updates to reward function

--- hzd_rl/rabbit_tools/reward.py
+++ hzd_rl/rabbit_tools/reward.py
@@ -2,10 +2,8 @@ from rabbit_tools import trajectory
 
 """Customized Reward Func"""
 def get_reward(observation, desired_vel, velocity, mode="default"):
-	# velocity = observation[7]
-	angle = observation[2]
 	if mode == "default":	
-		if abs(velocity-desired_vel) <= 1:
+		if abs(velocity-desired_vel) <= 0.05:
 			if velocity <= desired_vel:
 				velocity_reward = (velocity / desired_vel)**2
 			else:
@@ -15,15 +13,23 @@ def get_reward(observation, desired_vel, velocity, mode="default"):
 
 		lf_clr = trajectory.left_foot_height(observation[0:7])
 		rf_clr = trajectory.right_foot_height(observation[0:7])
-		clearance_reward = 0.5*(lf_clr-rf_clr)**2
-		# clearance_reward = abs(lf_clr-rf_clr)
-		#step_reward = 0.5*(observation[3]-observation[5])**2
+		# clearance_reward = 0.02*(lf_clr-rf_clr)**2
+		clearance_reward = abs(lf_clr-rf_clr)
+		# step_reward = abs(observation[3]-observation[5])
 
-		reward = 0.1 * (10 * velocity_reward + 1*clearance_reward# + 1*step_reward #+ 0.25*angle 
-
-		#reward = 0.1 * (10 * velocity_reward + 2*clearance_reward + 0.25*angle)   Did not work with abs(velocity-desired_vel) <= 1:  
-
+		ang_reward = observation[2]
+		w_reward = observation[9]
+		# ang = observation[2]
+		# if abs(ang - 0.3) < 0.2:
+		# 	ang_reward = 0.1
+		# else:
+		# 	ang_reward = 0
+		# if ang < 0:
+		# 	ang_reward = 0.1*ang
+		# else:
+		# 	ang_reward = 0
 		
+		reward = 0.1 * (10 * velocity_reward + 1*clearance_reward + 1*w_reward+ 1*ang_reward)# + 1*step_reward) #+ 0.25*angle
 	else:
 		raise NotImplementedError
 
